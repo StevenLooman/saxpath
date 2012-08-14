7364e2f05e0afd5a14e255e121fb7c94afb8b13b
Steven Looman <steven.looman@gmail.com>
2012-08-14 20:49:30 +0200

    .

--- run.js
+++ run.js
@@ -4,12 +4,12 @@ var fs  = require('fs');
 var sax = require('sax');
 var xps = require('./lib');
 
-var filename = 'test/test.xml';
+var filename = 'test/bookstore.xml';
 
 function main() {
     var saxParser = sax.createStream(true);
-    var streamer  = new xps.SaXPath(saxParser, '/bookstore/book');
-//    var streamer  = new xps.SaXPath(saxParser, '/bookstore/book[@category="COOKING"]');
+//    var streamer  = new xps.SaXPath(saxParser, '/bookstore/book');
+    var streamer  = new xps.SaXPath(saxParser, '/bookstore/book[@category="COOKING"]');
 
     var fileStream = fs.createReadStream(filename);
     fileStream.pipe(saxParser);
