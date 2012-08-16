8769ec312a9f3f4b0ddf1b7e4d7996dad2f6de86
Steven Looman <steven.looman@gmail.com>
2012-08-16 22:49:14 +0200

    Implemented recording of XML, sort of

--- run.js
+++ run.js
@@ -6,11 +6,17 @@ var xps = require('./lib');
 
 var filename = 'test/bookstore.xml';
 
+function onMatch(xml) {
+    console.log(xml);
+}
+
 function main() {
     var saxParser = sax.createStream(true);
 //    var streamer  = new xps.SaXPath(saxParser, '/bookstore/book');
 //    var streamer  = new xps.SaXPath(saxParser, '/bookstore/book[@category="COOKING"]');
     var streamer  = new xps.SaXPath(saxParser, '//book');
+
+    streamer.on('match', onMatch);
 
     var fileStream = fs.createReadStream(filename);
     fileStream.pipe(saxParser);
