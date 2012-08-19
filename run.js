9a0186d9e1bd0933183e458d8d87c1969701ca1c
Steven Looman <steven.looman@gmail.com>
2012-08-19 16:53:33 +0200

    Updated XmlRecorder to support multi-track-recording

--- run.js
+++ run.js
@@ -4,9 +4,11 @@ var fs  = require('fs');
 var sax = require('sax');
 var xps = require('./lib');
 
-var filename = 'test/bookstore.xml';
+//var filename = 'test/bookstore.xml';
+var filename = 'test/inception.xml';
 
 function onMatch(xml) {
+    console.log('=== on match');
     console.log(xml);
 }
 
@@ -14,7 +16,8 @@ function main() {
     var saxParser = sax.createStream(true);
 //    var streamer  = new xps.SaXPath(saxParser, '/bookstore/book');
 //    var streamer  = new xps.SaXPath(saxParser, '/bookstore/book[@category="COOKING"]');
-    var streamer  = new xps.SaXPath(saxParser, '//book');
+//    var streamer  = new xps.SaXPath(saxParser, '//book');
+    var streamer  = new xps.SaXPath(saxParser, '//node');
 
     streamer.on('match', onMatch);
 
