ce7d47e17f6006b055ad81de240743ad236f37f7
Steven Looman <steven.looman@gmail.com>
2013-08-30 14:44:06 +0200

    Added example.js, showing how to use SaXPath. Refs #5

---
+++ example.js
@@ -0,0 +1,14 @@
+var saxpath = require('./lib');
+var fs      = require('fs');
+var sax     = require('sax');
+
+var fileStream = fs.createReadStream('test/bookstore.xml');
+var saxParser  = sax.createStream(true);
+var streamer   = new saxpath.SaXPath(saxParser, '//book');
+
+streamer.on('match', function(xml) {
+    console.log('--- matched XML ---');
+    console.log(xml);
+});
+
+fileStream.pipe(saxParser);
