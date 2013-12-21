7c24c16fc607916b4cafba4cf4a1a6163813bcda
Steven Looman <steven.looman@gmail.com>
2013-12-21 10:49:15 +0100

    Minor changes to examples

--- examples/example.js
+++ examples/example.js
@@ -1,10 +1,11 @@
 var saxpath = require('../lib');
-var fs      = require('fs');
-var sax     = require('sax');
+var fs = require('fs');
+var sax = require('sax');
+
 
 var fileStream = fs.createReadStream('../test/bookstore.xml');
-var saxParser  = sax.createStream(true);
-var streamer   = new saxpath.SaXPath(saxParser, '//book');
+var saxParser = sax.createStream(true);
+var streamer = new saxpath.SaXPath(saxParser, '//book');
 
 streamer.on('match', function(xml) {
     console.log('--- matched XML ---');
